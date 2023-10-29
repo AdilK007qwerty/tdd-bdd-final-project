031b20cd699f31430879cb08c15a8467b3587abf
Your Name <you@example.com>
2023-10-29 03:16:17 -0400

    Completed Ex2

--- service/routes.py
+++ service/routes.py
@@ -106,9 +106,20 @@ def create_products():
 # R E A D   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
+@app.route("/products/<product_id>", methods=["GET"])
+def get_products(product_id):
+    """
+    Gets a Product
+    """
+    app.logger.info("Request to Retrieve a product with id [%s]", product_id)
+    product = Product.find(product_id)
+
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    
+    app.logger.info("Returning product: %s", product.name)
+    serialized = product.serialize()
+    return serialized, status.HTTP_200_OK
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
